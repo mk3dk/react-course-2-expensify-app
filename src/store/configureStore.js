3fe8ae574c1e838a8c84eaa0a48998e08323b550
Murat Yücel <murat.yucel@nordea.com>
2019-01-03 23:18:53 +0100

    first commit

---
+++ src/store/configureStore.js
@@ -0,0 +1,18 @@
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+    const store = createStore(
+        combineReducers({
+            expenses: expensesReducer,
+            filters: filtersReducer
+        }),
+        composeEnhancers(applyMiddleware(thunk))
+    );
+
+    return store;
+};
