5bc0d8cae7d8214b764e00f9c4999ae2529d5a9e
Murat Yücel <murat.yucel@nordea.com>
2019-01-04 16:01:19 +0100

    made it possible to edit and remove expenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -30,12 +30,30 @@ export const removeExpense = ({id} = {}) => ({
     id
 });
 
+// START_REMOVE_EXPENSE
+export const startRemoveExpense = ({id} = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({id}));
+        });
+    };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
+
+// START_EDIT_EXPENSE
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    };
+};
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
