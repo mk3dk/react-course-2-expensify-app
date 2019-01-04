2e13e662793d20692f1db5d815d12eddae538a23
Murat Yucel <muratyucel@Murats-MacBook-Air.local>
2019-01-04 21:56:04 +0100

    fixed test cases

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {startLogout} from '../actions/auth';
 
-const Header = ({startLogout}) => (
+export const Header = ({startLogout}) => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {LoginPage} from '../../components/LoginPage';
-import {Header} from "./Header.test";
-
-let startLogin, wrapper;
-
-beforeEach(() => {
-    startLogin = jest.fn();
-    wrapper = shallow(<LoginPage startLogin={startLogin}/>);
-});
 
 test('should correctly render LoginPage', () => {
+    const wrapper = shallow(<LoginPage startLogin={() => {}}/>);
     expect(wrapper).toMatchSnapshot();
 });
 
-test('should handle onSubmit', () => {
+test('should call startLogin on button click', () => {
+    const startLogin = jest.fn();
+    const wrapper = shallow(<LoginPage startLogin={startLogin}/>);
     wrapper.find('button').simulate('click');;
     expect(startLogin).toHaveBeenCalled();
 });
