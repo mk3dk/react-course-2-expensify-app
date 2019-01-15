037b5650082f5454b7617c7c1e7b8144dad9bfbb
Murat Yücel <murat.yucel@nordea.com>
2019-01-15 12:13:57 +0100

    added private routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, {history} from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {startSetExpenses} from './actions/expenses'
+import {login, logout} from './actions/auth'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
+
 
 const store = configureStore();
 
@@ -16,17 +18,28 @@ const jsx = (
         <AppRouter/>
     </Provider>
 );
+let hasRenderer = false;
+const renderApp = () => {
+    if (!hasRenderer) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRenderer = true;
+    }
+}
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
-});
-
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        console.log('Log in')
-    } else {
-        console.log('Log out')
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });        
+    } else {        
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
     }
 });
