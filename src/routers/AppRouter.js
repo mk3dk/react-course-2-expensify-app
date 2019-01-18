e4726d02df0d0e5a3865c3bc5beed5afd677672a
Murat Yücel <murat.yucel@nordea.com>
2019-01-18 19:44:35 +0100

    add expensify styles

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Route, Switch} from 'react-router-dom';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
@@ -12,17 +12,17 @@ import PublicRoute from './PublicRoute';
 export const history = createHistory();
 
 const AppRouter = () => (
-    <Router history={history}>
-        <div>            
-            <Switch>
-                <PublicRoute path="/" component={LoginPage} exact={true}/>
-                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
-                <PrivateRoute path="/create" component={AddExpensePage}/>
-                <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
-                <Route component={NotFoundPage}/>
-            </Switch>
-        </div>
-    </Router>
+  <Router history={history}>
+    <div>
+      <Switch>
+        <PublicRoute path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default AppRouter;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
@@ -7,14 +7,14 @@ import authReducer from '../reducers/auth';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
-    const store = createStore(
-        combineReducers({
-            expenses: expensesReducer,
-            filters: filtersReducer,
-            auth: authReducer
-        }),
-        composeEnhancers(applyMiddleware(thunk))
-    );
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer,
+      auth: authReducer
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
 
-    return store;
+  return store;
 };
